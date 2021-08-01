3632da2d5bc61cbd3814c46cc42ff31a3a610a6b
HayateArmy14 <pegadeli12345@gmail.com>
2021-08-01 18:58:49 +0300

    removed front-page styles 2

--- pages/index.js
+++ pages/index.js
@@ -14,30 +14,6 @@ const Index = ({ courses }) => {
 
   return (
     <>
-      {/* <section className="section-1">
-        {/* <img src={"home.jpg"} alt="home" /> */}
-      {/* <div className="color"></div>
-        <div className="right-side">
-          <div className="right-container above">
-            <h1 className="emuson-title" style={{ color: "white" }}>
-              EMUSON
-            </h1>
-            <p className="emuson-description">
-              is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book. It has survived not
-              only five centuries, bu
-            </p>
-            <p className="button-container">
-              <a href="#all-courses" className="btnn btn-to-all-courses">
-                Explore Our Courses
-              </a>
-            </p>
-          </div>
-          <div className="color-2"></div>
-        </div> */}
-      {/* </section> */}
       <h2 className=" text-center title-2  jumbotron square ">
         Check Out Our Newest Courses
       </h2>
